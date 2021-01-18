fe8d9b870bff5ee3b98a5e9b748ea237fb6770fd
Serge Rusakou <texa@tut.by>
2021-01-18 13:13:23 +0300

    working variant

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,41 @@
-console.log("Welcome to the Brain Games!");
+console.log("Welcome to the Brain Games!kkkk");
+
+//-----------------------------------------------
+//import { surfaceArea, square } from './math.js';
+import {square} from "./math.js";
+
+//const surfaceOfMars = surfaceArea(3390);
+//const surfaceOfMercury = surfaceArea(2440);
+const yearSquared = square(5);
+
+//console.log(surfaceOfMars);
+//console.log(surfaceOfMercury);
+console.log(yearSquared);
+//----------------------------------------------
+
+
+import readlineSync from 'readline-sync';
+const name = readlineSync.question('Your answer: ');
+
+
+
+
+
+
+
+
+
+/*
+var readlineSync = require('readline-sync');
+
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+
+// Handle the secret text (e.g. password).
+var favFood = readlineSync.question('What is your favorite food? ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+});
+console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+*/
+
